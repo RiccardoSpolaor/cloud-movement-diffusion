64cde43fe604dad1f135518f63fcf1e9ee1d79c4
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-09-23 21:01:39 +0200

    Add and update src modules for GRU-Unet2D

--- src/model.py
+++ src/model.py
@@ -1,10 +1,13 @@
 from pathlib import Path
+from typing import Tuple
 
 import wandb
 import fastcore.all as fc
 import torch
 from torch import nn
 from diffusers import UNet2DModel
+
+from .conv_gru import ConvGRU
 
 
 def init_unet(model):
@@ -81,6 +84,7 @@ def get_unet_params(
     model_name: str,
     num_frames: int,
     num_channels: int = 1,
+    is_gru: bool = False
     ) -> dict: # TODO parametrize the num frames and num channels
     """
     Get the parameters for a diffuser UNet2D model.
@@ -99,14 +103,14 @@ def get_unet_params(
         The parameters for the UNet2D diffuser model.
     """
     if model_name == "unet_small":
-        return dict(
+        params = dict(
             block_out_channels=(16, 32, 64, 128), # number of channels for each block
             norm_num_groups=8, # number of groups for the normalization layer
             in_channels=num_frames*num_channels, # number of input channels
             out_channels=num_channels, # number of output channels
             )
     elif model_name == "unet_big":
-        return dict(
+        params = dict(
             block_out_channels=(32, 64, 128, 256), # number of channels for each block
             norm_num_groups=8, # number of groups for the normalization layer
             in_channels=num_frames*num_channels, # number of input channels
@@ -114,6 +118,14 @@ def get_unet_params(
             )
     else:
         raise(f"Model name not found: {model_name}, choose between 'unet_small' or 'unet_big'")
+
+    if is_gru:
+        params.update(
+            input_dim=1,
+            input_size=(64, 64),
+            hidden_size=3)
+
+    return params
 
 class UNet2D(UNet2DModel, WandbModel):
     """Class of a UNet2D model that can be saved to wandb"""
@@ -139,3 +151,35 @@ class UNet2D(UNet2DModel, WandbModel):
             The output of the UNet2D model.
         """
         return super().forward(*args, **kwargs).sample ## Diffusers's UNet2DOutput class
+
+class UNet2DTemporalCondition(UNet2DModel, WandbModel):
+    def __init__(self, 
+                 *x,
+                 input_dim: int,
+                 input_size: Tuple[int, int], 
+                 hidden_size: int,
+                 device: str = "cuda",
+                 **kwargs):
+        super().__init__(*x, **kwargs)
+        init_unet(self)
+        self.temporal_encoder = ConvGRU(input_size=input_size,
+                                        input_dim=input_dim,
+                                        hidden_dim=hidden_size,
+                                        kernel_size=(3, 3),
+                                        num_layers=2,
+                                        dtype=torch.cuda.FloatTensor,
+                                        batch_first=True,
+                                        bias = True,
+                                        return_all_layers = False).to(device)
+
+    def forward(self, *x, **kwargs):
+        # TODO: consider case where x has more than one channel?
+        # Get first frames and add the channel dimension to them.
+        temporal_input = x[0][:, :-1].unsqueeze(2) # first three images
+        _, encoder_hidden_states = self.temporal_encoder(temporal_input.to(self.device))
+        conv_lstm_features = encoder_hidden_states[0][0].to(self.device)
+        noisy_frame = x[0][:, -1:]
+        #print(noisy_frame.shape)
+        noise_hidden_state = torch.cat([conv_lstm_features, noisy_frame], dim=1)
+
+        return super().forward(noise_hidden_state, timestep=x[1], **kwargs).sample ## Diffusers's UNet2DConditionModel class
